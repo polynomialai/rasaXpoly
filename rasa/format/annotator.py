dff746b8e48c6342f65e09393f830d0e6bf06d0b
bibhabasuPolynomial <105656702+bibhabasuPolynomial@users.noreply.github.com>
2022-06-10 13:27:39 +0530

    Update annotator.py

--- rasa/format/annotator.py
+++ rasa/format/annotator.py
@@ -5,9 +5,9 @@ def annotate_example(example : str, token : str,  entity : str, synonym: str) ->
     print(example)
     return example
 
-# def annotate_regex_example(example : str, token : str,  regex_entity : str) -> str:
-#     annotation_add = f'"entity": "{entity}", "value": "{synonym}"'
-#     print("Token in ",token,"\n")
-#     example = example.replace(token, f"[{token}]" )+ "{"+ f"{annotation_add}" + "}"
-#     print(example)
-#     return example
+def add_regex_annotation(example : str, token : str, regex_entity : str) -> str:    
+
+    example = example.replace(token, f"[{token}]" )+ f"({regex_entity})"
+    
+   
+    return example
