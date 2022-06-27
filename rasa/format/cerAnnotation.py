02f0584b798d5123e37a4c56b94dc44b7a23aa4b
deepak3784 <magiciandeepakgupta@gamil.com>
2022-06-27 15:14:42 +0530

    Changed Annotation Format

--- rasa/format/cerAnnotation.py
+++ rasa/format/cerAnnotation.py
@@ -1,23 +1,25 @@
 import requests
 
-example = "vechicles in madras within 50 m with gps status as stop for driver Rohn"
+example = "My work mail is deepak.g@polynmial.ai"
 
 url = 'https://entity-extract.herokuapp.com/hybrid'
 myobj ={
     "query": f"{example}",
-    "agent_name": "transo",
-    "pack_name": ""   #no packname need to be mentioned we are loading all packs by default
+    "agent_name": "transo",           #no packname need to be mentioned we are loading all packs by default
+    "pack_name": "",   
+    "threshold": 80
 }
 
 x = requests.post(url, json= myobj )
 req = x.json()
 tokens = {}
+
 for i in req["entities"]:
     #print(i['entityClass'] ,'\t',i['token'])
     tokens[i['entityClass']] = i['token']
-
+print(tokens)
 def annotateNewExample(example,tokens):
     for token,value in tokens.items():
-        example = example.replace(value, f"[{value}]" + "{"+ f"{token}" + "}")
+        example = example.replace(value, f"[{value}]" + "("+ f"{token}" + ")")
     return example
 newExample = annotateNewExample(example,tokens)
